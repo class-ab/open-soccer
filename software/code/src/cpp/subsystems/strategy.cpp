160e1749058309ea3e53290be167ebf851bf8f97
class-ab <aidenblanchfield@gmail.com>
2026-09-12 12:06:37 +1000

    2: self remote detection and split opponent var

--- software/code/src/cpp/subsystems/strategy.cpp
+++ software/code/src/cpp/subsystems/strategy.cpp
@@ -2,9 +2,11 @@
 #include "include/subsystems/localization.h"
 #include "include/subsystems/robot_state.h"
 #include "include/subsystems/vision.h"
+#include "include/subsystems/communication.h"
 #include "cpp\subsystems\localization.cpp"
 #include "cpp\subsystems\robot_state.cpp"
 #include "cpp\subsystems\vision.cpp"
+#include "cpp\subsystems\communication.cpp"
 
 
 ballLocation ballState = {false, 0, BallState::unknown};
@@ -23,8 +25,11 @@ void updateStrategy() {
 void updateBallState() {
     FieldBall ball; // get ball localisation data
     getFieldBall(ball);
-    RobotPose robot; // get robot localisation data 
-    getRobotPose(robot);
+    RobotPose robotPose; // get robot localisation data 
+    getRobotPose(robotPose);
+    RobotPose remotePose;
+    getRemoteRobotPose(remotePose);
+    RobotPose opponentRobot;
 
     // update ball state based on localization data
     if (ball.valid) {
@@ -33,22 +38,20 @@ void updateBallState() {
         ballState.sinceCurrent = 0.0;
         if (ball.distanceCm <= BALL_TARGET_DISTANCE_CM && ball.angleDeg < 5 && ball.angleDeg > -5) {
             ballState.ballState = BallState::mePossession;
-        } /* else if ( other robot ballState.ballState == "mePossession")
-        {
-            NEED TO GET COMMUNICATION HERE
-
-        }*/else if (abs(ball.xMm) < MIDDLE_ZONE_X && abs(ball.yMm) < MIDDLE_ZONE_Y) {
+        } else if (abs(ball.xMm) < MIDDLE_ZONE_X && abs(ball.yMm) < MIDDLE_ZONE_Y) {
             ballState.ballState = BallState::middle; // ball is in middle zone 
         } else if (abs(ball.yMm) > SIDE_ZONE_Y && ball.xMm < -MIDDLE_ZONE_X) {
-          ballState.ballState = BallState::farSidesOwn; // ball is in far sides zone AND near own goal
+            ballState.ballState = BallState::farSidesOwn; // ball is in far sides zone AND near own goal
         } else if (abs(ball.yMm) > SIDE_ZONE_Y) {
-           ballState.ballState = BallState::farSides; // ball is in far sides zone NOT near own goal
+            ballState.ballState = BallState::farSides; // ball is in far sides zone NOT near own goal
         } else if (ball.xMm < -MIDDLE_ZONE_X) {
-          ballState.ballState = BallState::nearOwnGoal; // ball is near own goal
+            ballState.ballState = BallState::nearOwnGoal; // ball is near own goal
         } else if (ball.xMm > MIDDLE_ZONE_X) {
-        ballState.ballState = BallState::nearFarGoal; // ball is near far goal
+            ballState.ballState = BallState::nearFarGoal; // ball is near far goal
+        } else if (ball.xMm ) {
+
         } else {
-        ballState.ballState = BallState::unknown; // cannot find ball most likely, or it is in an edge spot
+            ballState.ballState = BallState::unknown; // cannot find ball most likely, or it is in an edge spot
         }
     } else {
         // Ball is not valid - mark as not current
